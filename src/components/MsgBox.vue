3dd57284b60dc01efd6dd10a2f726598b601696a
houseBetter <youlan_xyz@163.com>
2018-05-22 21:19:10 +0800

    增加对话框组件

---
+++ src/components/MsgBox.vue
@@ -0,0 +1,171 @@
+<template>
+    <div class="dialog-list">
+            <template v-if="data.type === 'user'">
+                <router-link class="dialog-row" tag="div" to="/dialog">   
+                    <div class="dialog-row-icon">
+                        <img :src="data.userInfo[0].icon"/>
+                        <i class="icon-state">9</i>
+                    </div>
+                    <div class="dialog-row-title">
+                        <div class="row-title-nickname">{{data.userInfo[0].uname}}</div>
+                        <div class="row-title-msg">
+                                夜华:我试一试
+                        </div>
+                    </div>
+                    <div class="dialog-row-right">
+                        <div class="right-time">22:04</div>
+                        <div class="right-icon">
+                            <i class="iconfont icon-maikefeng-jingyin-tianchongsvg"></i>
+                        </div>
+                    </div>
+                </router-link>
+            </template>
+            <template v-else>
+                <router-link class="dialog-row" tag="div" to="/dialog">
+                    <div class="dialog-row-icon" ref="dialogRowIcon">
+                        <img 
+                            :src="user.icon" 
+                            v-bind:key="user.uid"
+                            v-for="user in data.userInfo">
+                    </div>
+                    <div class="dialog-row-title">
+                        <div class="row-title-nickname">{{data.groupInfo.gname}}</div>
+                        <div class="row-title-msg">
+                                夜华:我试一试
+                        </div>
+                    </div>
+                    <div class="dialog-row-right">
+                        <div class="right-time">22:04</div>
+                        <div class="right-icon">
+                            <i class="iconfont icon-maikefeng-jingyin-tianchongsvg"></i>
+                        </div>
+                    </div>
+                </router-link>
+            </template>
+
+    </div> 
+</template>
+<script>
+export default {
+    name: 'MsgBox',
+    props:['data'],
+    data(){
+        return {
+
+        }
+    },
+    created(){
+        // console.log($props['data']);
+    },
+    mounted(){
+        // console.log($props['data']);
+    }
+}
+</script>
+<style scoped>
+
+
+/* 对话框 */
+.dialog-list .dialog-row{
+    height: 64px;
+    padding: 8px;
+    background-color:#fff;
+    position: relative;
+}
+.dialog-list .dialog-row .dialog-row-icon{
+    float:left;
+    height: 48px;
+    width: 48px;
+    margin-right: 10px;
+    background-color:#dddbdb;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    /* flex-flow: row wrap; */
+    justify-content: flex-start;
+    align-items: flex-start;
+    align-content: flex-start;
+    overflow:hidden;
+}
+.dialog-list .dialog-row .dialog-row-icon .icon-state{
+    position: absolute;
+    top: 0px;
+    left: 46px;
+    width: 18px;
+    height: 18px;
+    line-height: 18px;
+    background-color:red;
+    border-radius: 9px; 
+    padding:0 4px;
+    color:#fff;
+    text-align: center;
+
+    
+}
+.dialog-list .dialog-row .dialog-row-icon img{
+    /* width: 48px; */
+    /* width:30%; */
+    width:29%;
+    height:auto;
+    /* height: 48px; */
+    border-radius: 5px;
+    margin:1px;
+    flex-grow:2;
+
+}
+.dialog-list .dialog-row .dialog-row-title{
+    float:left;
+}
+.dialog-list .dialog-row .dialog-row-title .row-title-nickname{
+    height: 25px;
+    line-height: 25px;
+    font-size: 16px;
+    color: #000;
+}
+.dialog-list .dialog-row .dialog-row-title .row-title-msg{
+    height: 23px;
+    line-height: 23px;
+    font-size: 14px;
+    color: #888;
+}
+.dialog-list .dialog-row-right{
+    float:right;
+}
+.dialog-list .dialog-row-right .right-time{
+    height: 25px;
+    color:#b8b8b8;
+    font:13px/1.5 Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, Noto Sans CJK SC, WenQuanYi Micro Hei, Arial, sans-serif;
+}
+.dialog-list .dialog-row-right .right-icon{
+    color:#b8b8b8;
+    text-align: right;
+}
+.dialog-row:not(:first-child)::before{
+    content: "";
+    position: absolute;
+    left: 8px;
+    top: 0;
+    right: 0px;
+    background-color: #b7b7b7;
+    height: 1px;
+    transform: scale(.5);
+    transform-origin: 0 0;
+    width: 200%;
+}
+.dialog-row:last-child::after{
+    content: "";
+    position: absolute;
+    left: 8px;
+    bottom: 0;
+    right: 0px;
+    background-color: #b7b7b7;
+    height: 1px;
+    transform: scale(.5);
+    transform-origin: 0 0;
+    width: 200%;
+}
+
+</style>
+
+
